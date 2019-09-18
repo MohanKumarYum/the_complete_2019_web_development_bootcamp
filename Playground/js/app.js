99844da390bc069bd9415f3e630eb326e9c54d5e
Mohan Kumar <MohanKumarYum@Gmail.com>
2019-09-19 06:25:13 +1000

    DOM solution challenge

--- Playground/js/app.js
+++ Playground/js/app.js
@@ -86,7 +86,7 @@
 // function bmiCalculator(weight, height) {
 
 //     return Math.floor(weight / (height * height));
-    
+
 // }
 
 // console.log(bmiCalculator(100, 1.73)); 
@@ -98,3 +98,90 @@ var bmi = bmiCalculator(65, 1.8);
 bmi should equal around 20 in this case.
 
 */
+
+/* Love Calculator */
+
+// function bmiCalculator (weight, height) {
+//     return Math.floor(weight / (height * height));
+// }
+
+// var bmi = bmiCalculator (60, 2);
+
+// if (bmi < 18.5) {
+//     console.log("Your BMI is " + bmi + ", so you are underweight");
+// }
+// if (bmi >= 18.5 && bmi <= 24.9) {
+//     console.log("Your BMI is " + bmi + ", so you have a normal weight.");
+// }
+// if (bmi > 24.9) {
+//     console.log("Your BMI is " + bmi + ", so you are overweight");
+// }
+
+// var guestList = [
+//     "Angela",
+//     "Jack",
+//     "Pam",
+//     "James",
+//     "Lara",
+//     "Mohan",
+//     "Kumar",
+//     "Shifali"
+// ];
+
+// var yourName = prompt("What is your name?");
+
+// if (guestList.includes(yourName)) {
+//     console.log("Welcome!");
+// } else {
+//     console.log("Sorry!, next time.");
+// }
+
+// var output = [];
+// var counter = 1;
+
+// function fizzBuzz() {
+
+//     var value;
+//     if (counter % 3 === 0 && counter % 5 === 0) value = "FizzBuzz"
+//     else if (counter % 3 === 0) value = "Fizz";
+//     else if (counter % 5 === 0) value = "Buzz";
+//     else value = counter;
+
+//     output.push(value);
+
+//     counter++;
+
+//     console.log(output);
+// }
+
+// fizzBuzz();
+
+
+// var maxCount = 99;
+// var counter = 99;
+
+// function Beer() {
+//     while (counter > 0) {
+//         console.log(counter + " bottles of beer on the wall, " + counter + " bottles of beer. Take one down and pass it around, " + (--counter) + " bottles of beer on the wall.");
+//     }
+
+//     console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, " + maxCount + " bottles of beer on the wall.")
+// }
+
+// function fibonacciGenerator(n) {
+//     var outputArr = [];
+
+//     var prevNumber = 0;
+
+//     if (n === 0) return console.log("Number has to be greater than 0");
+//     if (n === 1) return outputArr.push(0);
+
+//     outputArr.push(0);
+//     outputArr.push(1);
+
+//     for (var i = 2; i < n; i++) {
+//         outputArr[i] = outputArr[i - 2] + outputArr[i - 1];
+//     }
+
+//     return outputArr;
+// }
