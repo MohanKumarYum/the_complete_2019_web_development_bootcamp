f0e66339bd8300113588dfb887330c5604396dda
Mohan Kumar <MohanKumarYum@Gmail.com>
2019-09-21 22:23:53 +1000

    Object & Dot Notation

--- Challenges/constructor.js
+++ Challenges/constructor.js
@@ -1,8 +1,15 @@
-function HouseKeeper (yearsOfExperience, name, cleaningRepertoire){
-this.yearsOfExperience = yearsOfExperience;
-this.name = name;
-this.cleaningRepertoire = cleaningRepertoire;
+function HouseKeeper(yearsOfExperience, name, cleaningRepertoire) {
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
+    this.clean = function () {
+        console.log("Cleaning in progress...");
+    }
 }
 
 var houseKeeper1 = new HouseKeeper(5, "Lilly", ["toilet", "lobby"]);
-console.log('houseKeeper1:', houseKeeper1)
+console.log(houseKeeper1);
+
+var houseKeeper2 = new HouseKeeper(7, "Julie", ["hallway", "lobby"]);
+
+houseKeeper2.clean();
